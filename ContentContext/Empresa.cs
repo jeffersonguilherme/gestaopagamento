ce69055cefd7994e27d563bb9a1337a8686533be
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-12-21 12:36:07 -0300

    da

--- ContentContext/Empresa.cs
+++ ContentContext/Empresa.cs
@@ -13,16 +13,12 @@ namespace GestaoPagamento.ContentContext{
             NomeFantasia = nomeFantasia;
             Cnpj = cnpj;
             IncricaoEstatual = incricaoEstatual;
-            Enderecos = new List<Endereco>();
-            Contatos =new List<Contato>();
         }
         public string RazaoSocial { get; set; }
         public string NomeFantasia { get; set; }
         public string Cnpj { get; set; }
         public string IncricaoEstatual { get; set; }
-        public EcontentCategoria Categoria { get; set; }
-        public IList<Endereco> Enderecos {get; set;}
-        public IList<Contato> Contatos{get; set;}
+
 
     }
 }
